ba3012e34aea778223d543c6e87603d5f5576a75
Your Name <you@example.com>
2023-05-14 21:19:06 +0530

    save files

--- resources/user.py
+++ resources/user.py
@@ -1,28 +1,22 @@
+import os
+import requests
+from flask import current_app
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required, get_jwt
 from sqlalchemy import or_
 
-import os
-import requests
+
 from db import db
 from blocklist import BLOCKLIST
 from models import UserModel
 from schemas import UserSchema, UserRegisterSchema
+from tasks import send_user_registration_email
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
-def send_simple_message(to, subject, body):
-    domain = os.getenv("MAILGUN_DOMAIN")
-    return requests.post(
-		f"https://api.mailgun.net/v3/{domain}/messages",
-		auth=("api", os.getenv("MAILGUN_API_KEY")),
-		data={"from": f"naveen<mailgun@{domain}>",
-			"to": [to],
-			"subject": subject,
-			"text": body})
 
 @blp.route("/register")
 class UserRegister(MethodView):
@@ -43,11 +37,7 @@ class UserRegister(MethodView):
         db.session.add(user)
         db.session.commit()
         
-        send_simple_message(
-            to=user.email,
-            subject="Successfully signed up",
-            body=f"Hi {user.username}! You have successfully signed up to the Stores REST API."
-        )
+        current_app.queue.enqueue(send_user_registration_email, user.email, user.username)
 
         return {"message": "User created successfully."}, 201
 
