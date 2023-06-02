1cb8859c90288e26a6b10c452849523c57dec4ee
Your Name <you@example.com>
2023-06-02 21:13:29 +0530

    commit all files

--- models/store.py
+++ models/store.py
@@ -9,3 +9,30 @@ class StoreModel(db.Model):
     items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
     tags = db.relationship("TagModel", back_populates = "store", lazy ="dynamic")
     
+    
+# class ItemModel(db.Model):
+#     __tablename__ = "items"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), unique=False, nullable=False)
+#     description = db.Column(db.String)
+#     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
+#     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False)
+#     store = db.relationship("StoreModel", back_populates="items")
+#     tags = db.relationship("TagModel", back_populates="items", secondary="items_tags")
+    
+# class TagModel(db.Model):
+#     __tablename__ = "tags"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), unique=False, nullable=False)
+#     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), nullable=False)
+
+#     store = db.relationship("StoreModel", back_populates="tags")
+#     items = db.relationship("ItemModel", back_populates="tags", secondary="items_tags")
+# class ItemsTags(db.Model):
+#     __tablename__ = "items_tags"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
+#     tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"))
--- schemas.py
+++ schemas.py
@@ -54,3 +54,57 @@ class UserSchema(Schema):
 
 class UserRegisterSchema(UserSchema):
     email = fields.Str(required=True)
+    
+    
+# "example": {
+#             "name": "pni-1/2",
+#             "admin": "true",
+#             "description": "Ethernet-interface",
+#             "nativeVlanId": 1,
+#             "mtu": 80,
+#             "bandwidth": {
+#                 "uplink": 10,
+#                 "downlink": 20
+#             },
+#             "subInterface": [
+#                 {
+#                 "unitId": 1,
+#                 "description": "Sub-interface",
+#                 "vlanId": 22,
+#                 "networkType": "WAN",
+#                 "networkName": "WAN networks",
+#                 "mtu": 72,
+#                 "bandwidth": {
+#                     "uplink": 800,
+#                     "downlink": 900
+#                 },
+#                 "ipv4": {
+#                     "addressType": "Static",
+#                     "staticAddress": {
+#                     "ipAddressMask": [
+#                         "1.2.3.4/24"
+#                     ],
+#                     "staticArp": [
+#                         {
+#                         "subnetAddressMask": "1.2.3.4/24",
+#                         "macAddress": "aa:bb:cc:34:e5:44"
+#                         }
+#                     ]
+#                     }
+#                 },
+#                 "dhcpAddress": {
+#                     "routePreference": 3,
+#                     "vendorClassIdentifier": "aaa 1212 bbbb",
+#                     "reachabilityMonitor": {
+#                     "icmp": "True",
+#                     "interval": 1,
+#                     "threshold": 2
+#                     }
+                    
+#                 }
+#                 }
+#         ]
+#         }
+# }
+        
+    
